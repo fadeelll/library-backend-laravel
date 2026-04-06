379e03be5896ce49ec4df24ddeb5e03328694f58
fadeelll <fadeelmubarokwok08@gmail.com>
2026-04-06 09:35:32 +0700

    menambahkan file migration

--- database/migrations/2026_04_02_041539_add_cover_image_to_books_table.php
+++ database/migrations/2026_04_02_041539_add_cover_image_to_books_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
    public function up(): void
 {
     Schema::table('books', function (Blueprint $table) {
-        $table->string('cover_image')->nullable()->after('deskripsi'); // Tambahkan kolom ini
+        $table->string('cover_image')->nullable();
     });
 }
 
